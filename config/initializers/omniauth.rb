a8d6bebe7fe45a7161dd37a8e9256bbf743b35da
Steve Roettger <steve@steveroettger.com>
2012-05-22 10:45:16 -0400

    Added twitter omniauth credentials

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
-  provider :twitter, 'CONSUMER_KEY', 'CONSUMER_SECRET'
+  provider :twitter, 'zSI4bMf4jvVk54BGwjVtwQ', 'oGnxjkPQjURugPwQp59gdnIuAPvD50E3b9nAystY'
 end
